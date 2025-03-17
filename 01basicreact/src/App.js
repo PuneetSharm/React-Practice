3b169b885bf0a68374d2c4942e205a598497b67b
Puneet Kumar Sharma <spuneet37@gmail.com>
2025-03-17 14:13:03 +0530

    created react project using CRA and vite

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+   <>
+   <h1>React Practice with CRA | Puneet</h1>
+   </>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,12 @@
+
+
+
+function App() {
+  return (
+    <>
+    <h1>React Practice with vite | Puneet</h1>
+    </>
+  )
+}
+
+export default App
